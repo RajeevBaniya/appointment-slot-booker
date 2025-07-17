b74bda2064b0ef3e6bb98fb8e78286808296f822
RajeevBaniya <baniyarajeev811@gmail.com>
2025-07-17 23:18:41 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppointmentBooker from "./components/AppointmentBooker";
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-white">
+      <div className="w-full h-full px-4 py-8">
+        <AppointmentBooker />
+      </div>
+    </div>
+  );
+}
+
+export default App;
